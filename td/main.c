c0d69dcff9091ddb81f9fa159d01f16d3269e00a
Gabin Marignier <gabin.marignier@telecom-paristech.fr>
2016-01-02 15:12:13 +0100

    implémentation de fibonacci

--- td/main.c
+++ td/main.c
@@ -1,4 +1,10 @@
+int fibo(int n) {
+    if (n == 0 || n == 1)
+        return n;
+    else
+        return fibo(n-1) + fibo(n-2);
+}
+        
 int main() {
-    while(1) {}
-    return 0;
+    return fibo(8);
 }
