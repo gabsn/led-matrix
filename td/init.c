ddc7842700fe14db172998d4126e9effc828748c
Gabin Marignier <gabin.marignier@telecom-paristech.fr>
2016-01-04 09:17:04 +0100

    debug du init_bss()

--- td/init.c
+++ td/init.c
@@ -1,8 +1,8 @@
 #include <stdint.h>
 
-extern uint16_t _etext, _data, _edata, _rodata, _erodata, _bstart, _bend;
+extern uint8_t _etext, _data, _edata, _rodata, _erodata, _bstart, _bend;
 
-uint16_t *dst;
+uint8_t *dst;
 
 void init_bss() {
     for (dst = &_bstart; dst < &_bend; dst++) 
