cdd808cdb9344f0237989964ebf98f8aa0509b92
Gabin Marignier <gabin.marignier@telecom-paristech.fr>
2016-01-11 17:56:11 +0100

    implémentation de uart_putchar()

--- td/main.c
+++ td/main.c
@@ -1,9 +1,11 @@
-#include "include/led.h"
 #include "include/clocks.h"
+#include "include/led.h"
+#include "include/uart.h"
 
 int main() {
     clocks_init();
-    led_init();
+    uart_init();
+    uart_putchar('b');
 
     return 0;
 }
--- td/uart.c
+++ td/uart.c
@@ -1,47 +1,68 @@
 #include <stdint.h>
 
-#define SIM_SOPT2 *((volatile uint32_t *) 0x40048004) 
-#define SIM_SOPT5 *((volatile uint32_t *) 0x40048010) 
-#define SIM_SCGC4 *((volatile uint32_t *) 0x40048034) 
-#define UART0_C1 *((volatile uint8_t *) 0x4006a002)
-#define UART0_C4 *((volatile uint8_t *) 0x4006a00a) 
-#define UART0_BDH *((volatile uint8_t *) 0x4006a000) 
-#define UART0_BDL *((volatile uint8_t *) 0x4006a001) 
-#define PORTA_PCR1 *((volatile uint32_t *) 0x40049004)
-#define PORTA_PCR2 *((volatile uint32_t *) 0x40049008)
+#define SIM_SOPT2 (*(volatile uint32_t * const) 0x40048004) 
+#define SIM_SOPT5 (*(volatile uint32_t * const) 0x40048010) 
+#define SIM_SCGC4 (*(volatile uint32_t * const) 0x40048034) 
+#define SIM_SCGC5 (*(volatile uint32_t * const) 0x40048038) 
 
+#define UART0_C1 (*(volatile uint8_t * const) 0x4006a002)
+#define UART0_C2 (*(volatile uint8_t * const) 0x4006a003)
+#define UART0_C4 (*(volatile uint8_t * const) 0x4006a00a) 
+#define UART0_BDH (*(volatile uint8_t * const) 0x4006a000) 
+#define UART0_BDL (*(volatile uint8_t * const) 0x4006a001) 
+#define UART0_S1 (*(volatile uint8_t * const) 0x4006a004) 
+#define UART0_D (*(volatile uint8_t * const) 0x4006a007) 
+
+#define PORTA_PCR1 (*(volatile uint32_t * const) 0x40049004)
+#define PORTA_PCR2 (*(volatile uint32_t * const) 0x40049008)
 
 void uart_init() {
     // Select the clock source for the UART0
-    SIM_SOPT2 |= 1 << 26;
-    SIM_SCGC4 |= 1 << 10;
+    SIM_SOPT2 |= 0x4000000;
+    SIM_SCGC4 |= 0x400;
 
-    // Set oversampling to 28 times
-    UART0_C4 |= 28;
+    // Set clock to 24MHz
+    SIM_SOPT2 |= 1 << 16;
 
-    // Set UART to 8 bits
-    UART0_C4 &= 0 << 5;
-    UART0_C1 &= 0 << 4;
-
-    // Set baud rate
-    UART0_BDH &= 0xe0;
-    UART0_BDL = 7;
-
-    // Disable parity
-    UART0_C1 &= 0 << 1;
-
-    // Set one bit stop
-    UART0_BDH &= 0 << 5;
-
+    // Disable UART0
+    UART0_C2 &= ~0xC;
+    
     // Enable portA's clock 
-    SIM_SCGC5 |= 1 << 9;
+    SIM_SCGC5 |= 0x200;
 
     // Set mux to RX et TX
-    PORTA_PCR1 |= 2 << 8; 
-    PORTA_PCR2 |= 2 << 8;  
+    PORTA_PCR1 |= 0x200; 
+    PORTA_PCR2 |= 0x200;  
+
+    // Set oversampling to 29 times
+    UART0_C4 |= 29;
+
+    // Set baud rate
+    UART0_BDL = 7;
+    UART0_BDH = 0; 
+
+    UART0_BDH = 0; 
+    // Set UART to 8 bits + disable parity
+    UART0_C1 &= 0xed;
+    UART0_C4 &= 0xdf;
 
     // Active RX and TX
-    SIM_SOPT5 &= ~7; 
+    SIM_SOPT5 &= 0xfffffff8; 
+
+    // Enable UART0
+    UART0_C2 |= 0xc;
+}
+
+/*
+void uart_putchar(char c) {
+    while (!(UART0_S1 & (1 << 7)) || !(UART0_S1 & (1 << 6))) {}
+    UART0_D = c;
+}
+*/
+
+void uart_putchar(char c){
+    while(!(UART0_S1 & 0x80) && !(UART0_S1 & 0x40)){}
+        UART0_D = c;
 }
 
 
