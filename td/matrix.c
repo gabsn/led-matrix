a322ef4b21a1f8be4e740386ba9ab6b42465342c
Gabin Marignier <gabin.marignier@telecom-paristech.fr>
2016-01-20 16:49:34 +0100

    wip

--- td/matrix.c
+++ td/matrix.c
@@ -1,32 +1,22 @@
+#include "include/utils.h"
+#include <stdint.h>
+
 // Clocks register
 #define SIM_SCGC5 (*(volatile uint32_t * const) 0x40048038)
 
-// PA4
-#define C7 (*(volatile uint32_t * const) 0x40049010)
-// PA12
-#define C6 (*(volatile uint32_t * const) 0x40049030)
-// PA13
-#define C0 (*(volatile uint32_t * const) 0x40049034)
-// PB0
-#define SLB (*(volatile uint32_t * const) 0x4004a000)
-// PB1
-#define LAT (*(volatile uint32_t * const) 0x4004a004)
-// PB2
-#define RST (*(volatile uint32_t * const) 0x4004a008)
-// PC8
-#define SCK (*(volatile uint32_t * const) 0x4004b020)
-// PC9
-#define SDA (*(volatile uint32_t * const) 0x4004b024)
-// PD2
-#define C1 (*(volatile uint32_t * const) 0x4004c008)
-// PD4
-#define C2 (*(volatile uint32_t * const) 0x4004c010)
-// PD5
-#define C5 (*(volatile uint32_t * const) 0x4004c014)
-// PD6
-#define C3 (*(volatile uint32_t * const) 0x4004c018)
-// PD7
-#define C4 (*(volatile uint32_t * const) 0x4004c01C)
+#define PA4 (*(volatile uint32_t * const) 0x40049010)
+#define PA12 (*(volatile uint32_t * const) 0x40049030)
+#define PA13 (*(volatile uint32_t * const) 0x40049034)
+#define PB0 (*(volatile uint32_t * const) 0x4004a000)
+#define PB1 (*(volatile uint32_t * const) 0x4004a004)
+#define PB2 (*(volatile uint32_t * const) 0x4004a008)
+#define PC8 (*(volatile uint32_t * const) 0x4004b020)
+#define PC9 (*(volatile uint32_t * const) 0x4004b024)
+#define PD2 (*(volatile uint32_t * const) 0x4004c008)
+#define PD4 (*(volatile uint32_t * const) 0x4004c010)
+#define PD5 (*(volatile uint32_t * const) 0x4004c014)
+#define PD6 (*(volatile uint32_t * const) 0x4004c018)
+#define PD7 (*(volatile uint32_t * const) 0x4004c01C)
 
 // GPIO registers
 #define GPIOA_PDDR (*(volatile uint32_t * const) 0x400ff014)
@@ -47,7 +37,7 @@
 #define GPIOD_PCOR (*(volatile uint32_t *) 0x400ff0c8)
 
 // Define useful macro to set/clear register
-#define SLB(x) ((x == 0) ? GPIOB_PCOR |= 1 : GPIOB_PSOR |= 1) 
+#define SB(x) ((x == 0) ? GPIOB_PCOR |= 1 : GPIOB_PSOR |= 1) 
 #define LAT(x) ((x == 0) ? GPIOB_PCOR |= (1 << 1) : GPIOB_PSOR |= (1 << 1)) 
 #define RST(x) ((x == 0) ? GPIOB_PCOR |= (1 << 2) : GPIOB_PSOR |= (1 << 2)) 
 #define SCK(x) ((x == 0) ? GPIOC_PCOR |= (1 << 8) : GPIOC_PSOR |= (1 << 8)) 
@@ -66,18 +56,19 @@ void matrix_init() {
     SIM_SCGC5 |= 0x1e00;
 
     // Set mux to GPIO
-    C0 |= 0x100;
-    C1 |= 0x100;
-    C2 |= 0x100;
-    C3 |= 0x100;
-    C4 |= 0x100;
-    C5 |= 0x100;
-    C6 |= 0x100;
-    C7 |= 0x100;
-    SB |= 0x100;
-    LAT |= 0x100;
-    RST |= 0x100;
-    SDA |= 0x100;
+    PA13 |= 0x100;
+    PD2 |= 0x100;
+    PD4 |= 0x100;
+    PD6 |= 0x100;
+    PD7 |= 0x100;
+    PD5 |= 0x100;
+    PA12 |= 0x100;
+    PA4 |= 0x100;
+    PB0 |= 0x100;
+    PB1 |= 0x100;
+    PB2 |= 0x100;
+    PC9 |= 0x100;
+/* TODO PC8 */
 
     // Set Port Data Direction Register
     GPIOA_PDDR |= 0x3010;
@@ -86,4 +77,20 @@ void matrix_init() {
     GPIOD_PDDR |= 0xf4;
 
     // Initializing
+    RST(0);
+    LAT(1);
+    SB(1);
+    SCK(0);
+    SDA(0);
+    C0(0);
+    C1(0);
+    C2(0);
+    C3(0);
+    C4(0);
+    C5(0);
+    C6(0);
+    C7(0);
 
+    // Wait 500ms then continue
+    wait_for(500);
+    RST(1);
